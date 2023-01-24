6cb7ed18d19375b81df13a53265003ceb69952a8
Marcin Plewa <112558575+Cin-Hub@users.noreply.github.com>
2023-01-24 18:14:07 +0100

    Showing current internet IPs

---
+++ myip.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Showing current internet IPs
+
+curl https://ipv4.geojs.io/v1/ip https://ipv6.geojs.io/v1/ip
+
+# For more nice and slick features like geo location
+# visit: https://www.geojs.io
